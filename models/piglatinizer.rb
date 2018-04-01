cdc7c400ec6dcbb2a4fe7e999046bbd8330a030f
Antonio Reid <read.musik@gmail.com>
2018-04-01 19:11:43 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,7 +12,7 @@ end
     #if text.split(", ").size == 1
     #  if text[0].downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0 #begins with constant
     #    result = text[1..-1] + text[0] + "ay"
-    #  elsif text[0].downcase.scan(/[aeoui]/).size > 0 #begins with vowel 
+    #  elsif text[0].downcase.scan(/[aeoui]/).size > 0 #begins with vowel
     #    result = text + "way"
     #  end
     #end

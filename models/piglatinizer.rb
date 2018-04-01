7d494677bf91711a0bda473f141994c85a429f3f
Antonio Reid <read.musik@gmail.com>
2018-04-01 18:34:18 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,9 +3,9 @@ class PigLatinizer
   def piglatinize(text)
 
     if text.split(", ").size == 1
-      if text[0].downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0
+      if text[0].downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0 #begins with constant
         result = text[1..-1] + text[0] + "ay"
-      elsif text[0].downcase.scan(/[aeoui]/).size > 0
+      elsif text[0].downcase.scan(/[aeoui]/).size > 0 #begins with vowel 
         result = text + "way"
       end
     end

991a308958b5d7307fe7ee99b7bb494881d95cfa
Antonio Reid <read.musik@gmail.com>
2018-04-01 18:29:30 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,7 +5,7 @@ class PigLatinizer
     if text.split(", ").size == 1
       if text[0].scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0
         result = text[1..-1] + text[0] + "ay"
-      elsif text[0].scan(/[aeoui]/).size > 0
+      elsif text[0].downcase.scan(/[aeoui]/).size > 0
         result = text + "way"
       end
     end

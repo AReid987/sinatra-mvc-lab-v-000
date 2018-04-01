1ae8e6fb3ae6ce423b2e2faf9e91a8e957d018aa
Antonio Reid <read.musik@gmail.com>
2018-04-01 19:11:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,16 +1,20 @@
 class PigLatinizer
 
   def piglatinize(text)
-
-    if text.split(", ").size == 1
-      if text[0].downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0 #begins with constant
-        result = text[1..-1] + text[0] + "ay"
-      elsif text[0].downcase.scan(/[aeoui]/).size > 0 #begins with vowel 
-        result = text + "way"
-      end
-    end
+    text_arr = text.split(", ")
     binding.pry
-    result
   end
 
 end
+
+
+
+    #if text.split(", ").size == 1
+    #  if text[0].downcase.scan(/[bcdfghjklmnpqrstvwxyz]/).size > 0 #begins with constant
+    #    result = text[1..-1] + text[0] + "ay"
+    #  elsif text[0].downcase.scan(/[aeoui]/).size > 0 #begins with vowel 
+    #    result = text + "way"
+    #  end
+    #end
+
+    #result

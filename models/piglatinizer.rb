a430014708969a60d7d153f34316ebbebefa81fd
Antonio Reid <read.musik@gmail.com>
2018-04-01 18:23:35 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,13 @@
 class PigLatinizer
 
   def piglatinize(text)
-    suffix = "ay"
+  
     if text.split(", ").size == 1
-      result = text[1..-1] + text[0] + "ay"
+      if text[0].scan(/[bcdfghjklmnpqrstvwxyz]/) > 0
+        result = text[1..-1] + text[0] + "ay"
+      elsif text[0].scan(/[aeoui]/).size > 0
+        result = text[1..-1] + text[0] + "way"
+      end
     end
     binding.pry
     result
